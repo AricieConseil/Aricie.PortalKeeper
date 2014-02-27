79170f3a123b701456fb233c57db5184bcbacf17
Jean-Sylvain Boige <jsboige@users.noreply.github.com>
2014-02-27 15:20:41 +0000

    ObsoleteDNNProvider: supporting dnn6 with additional reflection

--- Aricie.DNN/Services/ObsoleteDNNProvider.vb
+++ Aricie.DNN/Services/ObsoleteDNNProvider.vb
@@ -80,15 +80,40 @@ Namespace Services
         End Sub
 
         Public Overridable Function AddOrUpdateFile(objFile As DotNetNuke.Services.FileSystem.FileInfo, content As Byte()) As Integer
-            Dim fileId As Integer = objFile.FileId
-            If fileId <= 0 Then
-                fileId = NukeHelper.FileController.AddFile(objFile)
+
+            If NukeHelper.DnnVersion.Major < 6 Then
+                Dim fileId As Integer = objFile.FileId
+                If fileId <= 0 Then
+                    fileId = NukeHelper.FileController.AddFile(objFile)
+                End If
+                If fileId > 0 Then
+                    NukeHelper.FileController.UpdateFileContent(fileId, content)
+                End If
+                Return fileId
+            Else
+                Dim params As Object() = {objFile.FileId}
+                Dim objFolder As FolderInfo = DirectCast(FolderManagerGetFolderMethod.Invoke(FolderManagerInstance, params), FolderInfo)
+                If objFolder IsNot Nothing Then
+                    Using objStream As New MemoryStream(content)
+                        If objFile.FileId > 0 Then
+                            params = {objFile, objStream}
+                            Return DirectCast(FileManagerUpdateFileMethod.Invoke(FileManagerInstance, params), Integer)
+                        Else
+                            params = {objFolder, objFile.FileName, objStream}
+                            Return DirectCast(FileManagerAddFileMethod.Invoke(FileManagerInstance, params), Integer)
+                        End If
+                    End Using
+                End If
             End If
-            If fileId > 0 Then
-                NukeHelper.FileController.UpdateFileContent(fileId, content)
-            End If
-            Return fileId
+            Return -1
         End Function
+
+
+       
+
+
+
+
 
         Public Overridable Function GetFolderFromPath(portalId As Integer, path As String) As FolderInfo
             Return NukeHelper.FolderController.GetFolder(portalId, path, False)
@@ -111,6 +136,103 @@ Namespace Services
         Public Overridable Function GetFiles(folderInfo As FolderInfo) As IEnumerable(Of DotNetNuke.Services.FileSystem.FileInfo)
             Return CBO.FillCollection(Of DotNetNuke.Services.FileSystem.FileInfo)(NukeHelper.FileController.GetFiles(folderInfo.PortalID, folderInfo.FolderID))
         End Function
+
+#Region "Private members"
+
+        Private _FolderManagerInstance As Object = Nothing
+
+        Private ReadOnly Property FolderManagerInstance As Object
+            Get
+                If _FolderManagerInstance Is Nothing Then
+                    Dim folderManagerType As Type = ReflectionHelper.CreateType("DotNetNuke.Services.FileSystem.FolderManager, DotNetNuke")
+                    _FolderManagerInstance = folderManagerType.GetProperty("Instance").GetValue(Nothing, Nothing)
+                End If
+                Return _FolderManagerInstance
+            End Get
+        End Property
+
+        Private _FolderManagerGetFolderMethod As MethodInfo
+
+        Public ReadOnly Property FolderManagerGetFolderMethod As MethodInfo
+            Get
+                If _FolderManagerGetFolderMethod Is Nothing Then
+                    Dim folderManagerType As Type = FolderManagerInstance.GetType()
+                    For Each objMethod As MethodInfo In folderManagerType.GetMethods()
+                        If objMethod.Name = "GetFolder" Then
+                            Dim objParameters As ParameterInfo() = objMethod.GetParameters()
+                            If objParameters.Count = 1 Then
+                                If objParameters(0).ParameterType Is GetType(Integer) Then
+                                    _FolderManagerGetFolderMethod = objMethod
+                                    Exit For
+                                End If
+                            End If
+                        End If
+                    Next
+                End If
+                Return _FolderManagerGetFolderMethod
+            End Get
+        End Property
+
+
+        Private _FileManagerInstance As Object = Nothing
+
+
+        Private ReadOnly Property FileManagerInstance As Object
+            Get
+                If _FileManagerInstance Is Nothing Then
+                    Dim fileManagerType As Type = ReflectionHelper.CreateType("DotNetNuke.Services.FileSystem.FileManager, DotNetNuke")
+                    _FileManagerInstance = fileManagerType.GetProperty("Instance").GetValue(Nothing, Nothing)
+                End If
+                Return _FileManagerInstance
+            End Get
+        End Property
+
+        Private _FileManagerUpdateFileMethod As MethodInfo
+
+        Public ReadOnly Property FileManagerUpdateFileMethod As MethodInfo
+            Get
+                If _FileManagerUpdateFileMethod Is Nothing Then
+                    Dim fileManagerType As Type = FileManagerInstance.GetType()
+                    For Each objMethod As MethodInfo In fileManagerType.GetMethods()
+                        If objMethod.Name = "UpdateFile" Then
+                            Dim objParameters As ParameterInfo() = objMethod.GetParameters()
+                            If objParameters.Count = 2 Then
+                                If objParameters(1).ParameterType Is GetType(Stream) Then
+                                    _FileManagerUpdateFileMethod = objMethod
+                                    Exit For
+                                End If
+                            End If
+                        End If
+                    Next
+                End If
+                Return _FileManagerUpdateFileMethod
+            End Get
+        End Property
+
+
+        Private _FileManagerAddFileMethod As MethodInfo
+
+        Private ReadOnly Property FileManagerAddFileMethod As MethodInfo
+            Get
+                If _FileManagerAddFileMethod Is Nothing Then
+                    Dim fileManagerType As Type = FileManagerInstance.GetType()
+                    For Each objMethod As MethodInfo In fileManagerType.GetMethods()
+                        If objMethod.Name = "AddFile" Then
+                            Dim objParameters As ParameterInfo() = objMethod.GetParameters()
+                            If objParameters.Count = 3 Then
+                                If objParameters(2).ParameterType Is GetType(Stream) Then
+                                    _FileManagerAddFileMethod = objMethod
+                                    Exit For
+                                End If
+                            End If
+                        End If
+                    Next
+                End If
+                Return _FileManagerAddFileMethod
+            End Get
+        End Property
+
+#End Region
 
 
     End Class
