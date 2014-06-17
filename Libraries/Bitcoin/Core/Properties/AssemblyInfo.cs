026b5796cbfacdff82602f75dbb146d70036723f
Aricie_cp <SND\Aricie_cp>
2014-06-17 09:37:32 +0000

    Version update

--- Libraries/Bitcoin/Core/Properties/AssemblyInfo.cs
+++ Libraries/Bitcoin/Core/Properties/AssemblyInfo.cs
@@ -3,14 +3,14 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyCompany("Aricie")]
+[assembly: AssemblyCopyright("Copyright © Aricie 2014")]
+[assembly: AssemblyDescription("Portal Keeper Assembly dedicated to Bitcoin")]
+[assembly: AssemblyFileVersion("04.00.00")]
 [assembly: AssemblyProduct("Aricie.PortalKeeper.Bitcoin")]
 [assembly: AssemblyTitle("Aricie.PortalKeeper.Bitcoin")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("04.00.00")]
 [assembly: CompilationRelaxations(8)]
 [assembly: ComVisible(false)]
 [assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
--- Libraries/Bitcoin/Strategies/Properties/AssemblyInfo.cs
+++ Libraries/Bitcoin/Strategies/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Aricie.PortalKeeper.Bitcoin.Strategies")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Portal Keeper Assembly dedicated to Bitcoin Strategies")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Aricie")]
 [assembly: AssemblyProduct("Aricie.PortalKeeper.Bitcoin.Strategies")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Copyright © Aricie 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("04.00.00")]
+[assembly: AssemblyFileVersion("04.00.00")]
