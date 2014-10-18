980902282ca7060d09a32c6d02f8aef61f0307e6
Aricie_cp <SND\Aricie_cp>
2014-10-18 00:29:22 +0000

    4.0.2 + release notes

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("04.00.00")> 
-<Assembly: AssemblyFileVersion("04.00.00")> 
+<Assembly: AssemblyVersion("04.00.02")> 
+<Assembly: AssemblyFileVersion("04.00.02")> 
