2b877beeab8b81d894913991a493f4eca1710e0b
Aricie_cp <SND\Aricie_cp>
2014-11-10 15:33:30 +0000

    Merge from Aricie.Shared

---
+++ Aricie.Shared/Libs/Aricie.Documents/Services/OpenXML/xlsxColumnInfo.vb
@@ -0,0 +1,15 @@
+﻿Imports DocumentFormat.OpenXml.Spreadsheet
+
+Namespace Services.OpenXML
+
+    Public Class xlsxColumnInfo
+
+        Public Property Name() As String
+        Public Property FriendlyName() As String
+        Public Property Format() As System.Type
+        Public Property Width() As Double = 0
+        'Public Property Autosize As Boolean = False
+
+    End Class
+
+End Namespace
