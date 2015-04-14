8c4617fc0058591533bd046fe4d2a7c0aada97a0
Aricie_cp <SND\Aricie_cp>
2015-04-14 12:40:18 +0000

    reintroduced legacy interface + fixed issue with debuginfo IP addresses

--- Aricie.Shared/Libs/Aricie.DNN/Services/Tokens/IContextProvider.vb
+++ Aricie.Shared/Libs/Aricie.DNN/Services/Tokens/IContextProvider.vb
@@ -1,12 +1,29 @@
 ﻿Namespace Services
 
+
     ''' <summary>
-    ''' Interface for context providers: not used
+    ''' Interface for context providers: obsolete (restored for compatibility)
     ''' </summary>
     ''' <remarks></remarks>
+    <Obsolete("User IContextSource instead")> _
     Public Interface IContextProvider
 
-        Function HasContect(objType As Type) As Boolean
+        Function GetCurrentContext() As Object
+
+        Function GetAvailiableContexts() As List(Of Object)
+
+    End Interface
+
+
+
+
+    ''' <summary>
+    ''' Interface for context providing contexts by type
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Interface IContextSource
+
+        Function HasContext(objType As Type) As Boolean
         Function GetContext(objType As Type) As Object
 
     End Interface
