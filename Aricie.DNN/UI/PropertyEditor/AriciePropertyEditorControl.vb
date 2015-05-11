7eeb03eb3c19e778b111e673bf50f1ddd5804b9d
Jean-Sylvain Boige <jsboige@users.noreply.github.com>
2015-05-11 14:36:15 +0000

    Merge from Shared

--- Aricie.DNN/UI/PropertyEditor/AriciePropertyEditorControl.vb
+++ Aricie.DNN/UI/PropertyEditor/AriciePropertyEditorControl.vb
@@ -704,7 +704,7 @@ Namespace UI.WebControls
                     If Page.Header.FindControl(cssId) Is Nothing Then
                         Dim lnk As New HtmlControls.HtmlLink
                         lnk.ID = cssId
-                        lnk.Href = "http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/flick/jquery-ui.css"
+                        lnk.Href = "https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/flick/jquery-ui.css"
                         lnk.Attributes.Add("type", "text/css")
                         lnk.Attributes.Add("rel", "stylesheet")
                         Page.Header.Controls.Add(lnk)
