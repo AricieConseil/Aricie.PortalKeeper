2cafde0681a2fb9c04f388c80b8cc3d75e3b5cd7
Jean-Sylvain Boige <jsboige@users.noreply.github.com>
2015-09-03 08:39:52 +0000

    Merge from Shared

--- Aricie.Documents/My Project/AssemblyInfo.vb
+++ Aricie.Documents/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.8.6")> 
-<Assembly: AssemblyFileVersion("1.8.6")> 
+<Assembly: AssemblyVersion("01.08.07")> 
+<Assembly: AssemblyFileVersion("01.08.07")> 
