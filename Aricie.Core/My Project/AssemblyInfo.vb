dcbdba692ed7f98705f5953addc44f54adb62a81
Jean-Sylvain Boige <jsboige@users.noreply.github.com>
2015-09-16 12:32:54 +0000

    version 4.2.2, added target path to signature action

--- Aricie.Core/My Project/AssemblyInfo.vb
+++ Aricie.Core/My Project/AssemblyInfo.vb
@@ -32,7 +32,7 @@ Imports System.Resources
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("01.08.07")> 
-<Assembly: AssemblyFileVersion("01.08.07")> 
+<Assembly: AssemblyVersion("01.08.08")> 
+<Assembly: AssemblyFileVersion("01.08.08")> 
 
 <Assembly: NeutralResourcesLanguage("")> 
