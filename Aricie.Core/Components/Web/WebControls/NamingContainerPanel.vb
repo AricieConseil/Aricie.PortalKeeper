494ae701ee525ab5830c1bdc58338a24ac708874
Jean-Sylvain Boige <jsboige@users.noreply.github.com>
2015-10-05 16:00:32 +0000

    Fixed multiple naming container issues (button panel + recursive entities)

---
+++ Aricie.Core/Components/Web/WebControls/NamingContainerPanel.vb
@@ -0,0 +1,11 @@
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+
+Namespace Web.UI.Controls
+    Public Class NamingContainerPanel
+        Inherits Panel
+        Implements INamingContainer
+
+
+    End Class
+End NameSpace
