0fcd84357ce768e06b845e9f25f64124ec28d5fb
Jean-Sylvain Boige <jsboige@users.noreply.github.com>
2015-10-20 14:51:08 +0000

    Edit and Continue extended to FCNMode=0

--- Aricie.Core/My Project/AssemblyInfo.vb
+++ Aricie.Core/My Project/AssemblyInfo.vb
@@ -32,7 +32,7 @@ Imports System.Resources
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("01.09.00")> 
-<Assembly: AssemblyFileVersion("01.09.00")> 
+<Assembly: AssemblyVersion("01.09.01")> 
+<Assembly: AssemblyFileVersion("01.09.01")> 
 
 <Assembly: NeutralResourcesLanguage("")> 
